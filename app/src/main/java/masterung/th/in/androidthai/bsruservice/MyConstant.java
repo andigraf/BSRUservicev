a30578b86e3050bfeb805e58d710310098e35ecb
andigraf <narawut4@hotmail.com>
2019-03-05 16:04:04 +0700

    Initial commit

---
+++ app/src/main/java/masterung/th/in/androidthai/bsruservice/MyConstant.java
@@ -0,0 +1,16 @@
+package masterung.th.in.androidthai.bsruservice;
+
+public class MyConstant {
+
+    private String urlAddUser = "https://www.androidthai.in.th/bsru/addUserMaster.php";
+    private String urlGetUserWhereUser = "https://www.androidthai.in.th/bsru/getUserWhereUserMaster.php";
+
+
+    public String getUrlGetUserWhereUser() {
+        return urlGetUserWhereUser;
+    }
+
+    public String getUrlAddUser() {
+        return urlAddUser;
+    }
+}
---
+++ app/src/main/java/masterung/th/in/androidthai/bsruservice/ServiceActivity.java
@@ -0,0 +1,90 @@
+package masterung.th.in.androidthai.bsruservice;
+
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+
+
+public class ServiceActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service);
+
+//        Create Toolbar
+        createToolbar();
+
+//        Map Activity
+        mapController();
+
+
+    }   // Main Method
+
+    private void mapController() {
+        TextView textView = findViewById(R.id.txtShowMap);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(ServiceActivity.this, MapsActivity.class);
+                startActivity(intent);
+                drawerLayout.closeDrawers();
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    private void createToolbar() {
+        toolbar = findViewById(R.id.toolbarService);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(R.drawable.ic_action_hambaker);
+
+        drawerLayout = findViewById(R.id.layoutDrawer);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(ServiceActivity.this,
+                drawerLayout, R.string.open, R.string.close);
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+    }
+
+}   // Main Class
